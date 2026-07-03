a3be3b6aa2f4c2a5cb0f5b7446009be35b20abbb
officialwork-design <a.officialwork217@gmail.com>
2026-07-03 15:16:21 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   APP_NAME: 'shift-image-manager',
-  GAS_WEB_APP_URL: '',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyzPkF0QjFntEMFAdgZ9sbAId-fczgvuiNEVn13jWnXLB9z1GZSHMRUS6l6rISRGVuT/exec',
   API_MODE: 'jsonp',
   DEFAULT_STORE: 'KABUKI',
   STORES: ['KABUKI', 'AKIBA'],
