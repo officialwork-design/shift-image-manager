5066c350569ed13d79e4773d8ab32a09b897b9ba
N ANDREANSYAH <harleyy883+work@gmail.com>
2026-08-19 11:09:14 +0900

    Update GAS web app URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   APP_NAME: 'shift-image-manager',
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyzPkF0QjFntEMFAdgZ9sbAId-fczgvuiNEVn13jWnXLB9z1GZSHMRUS6l6rISRGVuT/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxN90blFI5qpAhRElqP2AaLkAU2cpe-UIuwMpkyF1lMUlcfDtfBJzpngkzfJZ1Qfxg/exec',
   API_MODE: 'jsonp',
   DEFAULT_STORE: 'KABUKI',
   STORES: ['KABUKI', 'AKIBA'],
