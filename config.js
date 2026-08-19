30965ceaafe721b73e88dd7ab052744380d17c68
N ANDREANSYAH <harleyy883+work@gmail.com>
2026-08-19 11:58:20 +0900

    Fix GAS web app image lookup deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   APP_NAME: 'shift-image-manager',
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbz7gHwskOkUWZKNU7RHRVgblw46ozvubDkyF0Z6850bHhzkrC_9rqlof7CnyMYg5orn/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbz-NSM70nThVEJkTZ3FUaf81pwZD-ykLvXbjfHOM_Nnf6sUSiHt8JK_D5TnQ4A9InU_/exec',
   API_MODE: 'jsonp',
   DEFAULT_STORE: 'KABUKI',
   STORES: ['KABUKI', 'AKIBA'],
